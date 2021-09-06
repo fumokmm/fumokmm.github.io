1ccc630d8bc014a4722624dde2ef64440cfd9e7a
fumokmm <fumo.kmm@gmail.com>
2021-09-06 17:50:36 +0900

    更新履歴、ALL、トップページの更新日の連動

--- _scripts/check_updated.rb
+++ _scripts/check_updated.rb
@@ -57,6 +57,7 @@ class ReplaceUpdated
     buffer = File.open(@file_path, 'r') { |f| f.read() }
     buffer.gsub!(/^updated: *#{@old_updated}.*$/, "updated: #{@new_updated}")  
     File.open(@file_path, 'w') { |f| f.write(buffer) }
+    puts "現状: #{@old_updated} => 更新後: #{@new_updated} -- #{@file_path}"
   end
 end
 
@@ -239,7 +240,69 @@ end
 # 結果出力
 [info1, info2, info3, info4].flatten.each do |info|
   if info['updated_orig'] < info['updated']
-    puts "現状: #{info['updated_orig']} => 更新後: #{info['updated']} -- #{info['path']}"
     ReplaceUpdated.new(info['path'], info['updated_orig'], info['updated']).exec
   end
+end
+
+###########################################
+# 更新履歴ページの更新日を更新
+def get_history_last_updated(col_dir)
+  history_dir = Dir.new(File.join(col_dir, '_history'))
+  Dir.glob(history_dir.path + '/**/*').map{ |f|
+    read_updated(f) if not File.directory?(f)
+  }.filter{ |d| d != nil }.max
+end
+history_index_file_path = File.join(base_dir, 'history/index.md')
+new_history_updated = get_history_last_updated(collections_dir)
+old_history_updated = read_updated(history_index_file_path)
+if old_history_updated < new_history_updated
+  ReplaceUpdated.new(history_index_file_path, old_history_updated, new_history_updated).exec
+end
+
+###########################################
+# Allの各ページの更新日を更新
+def get_all_last_updated(col_dir)
+  [
+    File.join(col_dir, '_it_articles/index.md'),
+    File.join(col_dir, '_life_articles/index.md'),
+    File.join(col_dir, '_reading_articles/index.md'),
+    File.join(col_dir, '_science_articles/index.md'),
+    File.join(col_dir, '_economy_articles/index.md')
+  ]
+  .map { |f| read_updated(f) }.max
+end
+new_all_updated = get_all_last_updated(collections_dir)
+all_index_files = [
+  File.join(base_dir, 'all/created_desc.md'),
+  File.join(base_dir, 'all/created.md'),
+  File.join(base_dir, 'all/index.md'),
+  File.join(base_dir, 'all/id.md'),
+  File.join(base_dir, 'all/updated.md')
+].each{ |all_index_file|
+  old_all_updated = read_updated(all_index_file)
+  if old_all_updated < new_all_updated
+    ReplaceUpdated.new(all_index_file, old_all_updated, new_all_updated).exec
+  end
+}
+
+###########################################
+# トップページの更新日を更新
+def get_main_categories_last_updated(base_dir, col_dir)
+  [
+    File.join(col_dir, '_it_articles/index.md'),
+    File.join(col_dir, '_life_articles/index.md'),
+    File.join(col_dir, '_reading_articles/index.md'),
+    File.join(col_dir, '_science_articles/index.md'),
+    File.join(col_dir, '_economy_articles/index.md'),
+    File.join(base_dir, 'history/index.md'),
+    File.join(base_dir, 'all/index.md'),
+    File.join(base_dir, 'about/index.md')
+  ]
+  .map { |f| read_updated(f) }.max
+end
+top_index_file_path = File.join(base_dir, 'index.md')
+new_top_updated = get_history_last_updated(collections_dir)
+old_top_updated = read_updated(top_index_file_path)
+if old_top_updated < new_top_updated
+  ReplaceUpdated.new(top_index_file_path, old_top_updated, new_top_updated).exec
 end
